df2159ae08a782a6c1508b4617ff5aa110bf357c
Tommy Dang <ledangt310@live.com>
2023-03-11 15:14:54 -0800

    Score tracking added

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const coinArr = ['Heads', 'Tails'];
 const message = document.querySelector('.message');
 const buttons = document.querySelectorAll('button');
-console.log(buttons);
+let score = [0, 0];
 
 for (let i = 0; i < buttons.length; i++) {
-  console.log(buttons[i]);
   buttons[i].addEventListener('click', gilToss);
 }
 
 function gilToss(e) {
-  console.log('Player: ' + e.target.innerText);
-  let compuntahToss = Math.floor(Math.random() * 2);
-  console.log('Computer: ' + coinArr[compuntahToss]);
+  let playerGuess = e.target.innerText;
+  let compuntahToss = Math.floor(Math.random() * coinArr.length);
+  let compuntahGuess = coinArr[compuntahToss];
+  message.innerHTML = `Computer Selected ${compuntahGuess} <br>`;
+  let outPut;
+
+  if (playerGuess === compuntahGuess) {
+    outPut = 'Player Wins';
+    score[0]++;
+  } else {
+    outPut = 'Computer Wins';
+    score[1]++;
+  }
+  message.innerHTML +=
+    outPut + '<br>Play ' + score[0] + ' Computer ' + score[1];
 }
